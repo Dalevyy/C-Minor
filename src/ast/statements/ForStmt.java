1c7c62a1c454698f39897f00814f2aefa8e57d13
Dalev <dalevyy@gmail.com>
2025-04-04 22:52:31 -0700

    Changed ForStmt to handle new ForStmt redesign.

--- src/ast/statements/ForStmt.java
+++ src/ast/statements/ForStmt.java
@@ -2,6 +2,7 @@ package ast.statements;
 
 import ast.*;
 import ast.expressions.*;
+import ast.operators.LoopOp;
 import token.*;
 import utilities.Visitor;
 import utilities.SymbolTable;
@@ -10,26 +11,33 @@ public class ForStmt extends Statement {
 
     public SymbolTable symbolTable;
 
-    private Vector<LocalDecl> forVars;
-    private Expression cond;
+    private LocalDecl loopControlVar;
+    private Expression LHS;
+    private Expression RHS;
+    private LoopOp lOp;
     private Statement nextExpr;
     private BlockStmt body;
 
-    public ForStmt(Token t, Vector<LocalDecl> fv, Expression c, Statement ne, BlockStmt b) {
+    public ForStmt(Token t, LocalDecl ld, Expression LHS, Expression RHS, LoopOp lOp, BlockStmt b) {
         super(t);
-        this.forVars = fv;
-        this.cond = c;
-        this.nextExpr = ne;
+        this.loopControlVar = ld;
+        this.LHS = LHS;
+        this.RHS = RHS;
+        this.lOp = lOp;
         this.body = b;
 
-        addChild(this.cond);
-        addChild(this.nextExpr);
+        addChild(this.loopControlVar);
+        addChild(this.LHS);
+        addChild(this.RHS);
+        addChild(this.lOp);
         addChild(this.body);
         setParent();
     }
 
-    public Vector<LocalDecl> forInits() { return forVars; }
-    public Expression condition() { return cond; }
+    public LocalDecl loopVar() { return this.loopControlVar; }
+    public Expression condLHS() { return this.LHS; }
+    public Expression condRHS() { return this.RHS; }
+    public LoopOp loopOp() { return this.lOp; }
     public Statement nextExpr() { return nextExpr; }
     public BlockStmt forBlock() { return body; }
 
