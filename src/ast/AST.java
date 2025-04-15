91a87b2435efe831e7fa34f2a4826396c1bbaed4
Dalev <dalevyy@gmail.com>
2025-04-15 12:25:45 -0700

    Added helper method to retrieve a node's type.

--- src/ast/AST.java
+++ src/ast/AST.java
@@ -139,8 +139,23 @@ public abstract class AST {
 
     public Vector asVector() { throw new RuntimeException("Expression can not be casted into a Vector.\n"); }
 
-    public static boolean notNull(AST n) { return n != null;}
+    // getType: Helper method to get a node's type (if applicable)
+    public Type getType() {
+        if(this.isExpression()) { return this.asExpression().type; }
+        else if(this.isParamDecl()) { return this.asParamDecl().type(); }
+        else if(this.isStatement()) {
+            if (this.asStatement().isLocalDecl()) { return this.asStatement().asLocalDecl().type();}
+            else { return null; }
+        }
+        else if(this.isTopLevelDecl()) {
+            if(this.asTopLevelDecl().isGlobalDecl()) { return this.asTopLevelDecl().asGlobalDecl().type(); }
+            else if(this.asTopLevelDecl().isEnumDecl()) { return this.asTopLevelDecl().asEnumDecl().constantType(); }
+            else { return null; }
+        }
+        else if(this.isFieldDecl()) { return this.asFieldDecl().type(); }
+        else { return null; }
 
+    }
     /*
     ----------------------------------------------------------------------
                                Visitor Methods
