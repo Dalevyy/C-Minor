5059a7de1bbb9fd59e3977205b411ba6a8186c37
Dalev <dalevyy@gmail.com>
2025-04-15 11:52:47 -0700

    Fixed support for declaring and accessing multidimensional arrays.

--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -41,9 +41,25 @@ public class Interpreter extends Visitor {
     _____________________________________________________________________
     */
     public void visitArrayExpr(ArrayExpr ae) {
+        LocalDecl arrayDecl = currentScope.findName(ae.arrayTarget().toString()).decl().asStatement().asLocalDecl();
         ArrayList<Object> arr = (ArrayList<Object>) stack.getValue(ae.arrayTarget().toString());
-        currValue = arr.get(Integer.parseInt(ae.arrayIndex().toString())-1);
+
+        int index = 0;
+        Vector<Expression> dims = arrayDecl.var().init().asArrayLiteral().arrayDims();
+        for(int i = 0; i < ae.arrayIndex().size(); i++) {
+            ae.arrayIndex().get(i).visit(this);
+            int currOffset = (int) currValue-1;
+            for(int j = i+1; j < dims.size(); j++) {
+                dims.get(i).visit(this);
+                currOffset *= (int) currValue;
+            }
+            index += currOffset;
+        }
+
+        currValue = arr.get(index);
     }
+
+    //
 
     /*
     _________________________ Array Literals _________________________
@@ -57,7 +73,10 @@ public class Interpreter extends Visitor {
 
         for(int i = 0; i < al.arrayInits().size(); i++) {
             al.arrayInits().get(i).visit(this);
-            arr.add(currValue);
+            if(currValue instanceof ArrayList) {
+                ArrayList<Object> smallArr = (ArrayList<Object>) currValue;
+                for(int j = 0; j < smallArr.size(); j++) { arr.add(smallArr.get(j)); }
+            } else { arr.add(currValue); }
         }
 
         currValue = arr;
@@ -81,9 +100,6 @@ public class Interpreter extends Visitor {
         String aOp = as.assignOp().toString();
 
         if(as.LHS().isArrayExpr()) {
-            String arrName = as.LHS().asArrayExpr().arrayTarget().toString();
-            ArrayList<Object> arr = (ArrayList<Object>) stack.getValue(arrName);
-            arr.set(Integer.parseInt(as.LHS().asArrayExpr().arrayIndex().toString())-1,newValue);
             return;
         }
 
@@ -709,14 +725,17 @@ public class Interpreter extends Visitor {
         for(String s : vals.keySet()) { stack.setValue(s,vals.get(s)); }
     }
 
-//    public void visitListLiteral(ListLiteral li) {
-//        ArrayList<Object> arr = new ArrayList<Object>();
-//        for(int i = 0; i < li.exprs().size(); i++) {
-//            li.exprs().get(i).visit(this);
-//            arr.add(currExpr.getValue(stack));
-//        }
-//        currExpr = li;
-//    }
+    public void visitListLiteral(ListLiteral li) {
+
+        ArrayList<Object> lst = new ArrayList<>();
+
+        for(int i = 0; i < li.inits().size(); i++) {
+            li.inits().get(i).visit(this);
+            lst.add(currValue);
+        }
+
+        currValue = lst;
+    }
 
     /*
     ________________________ Literals ________________________
