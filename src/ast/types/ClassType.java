8d5efa617e2989a0f1980cd27b5d0d88a13945a0
Dalev <dalevyy@gmail.com>
2025-05-04 11:41:11 -0700

    Added the ability for constructors to generate new vectors instead of setting them to null.

--- src/ast/types/ClassType.java
+++ src/ast/types/ClassType.java
@@ -17,8 +17,8 @@ public class ClassType extends Type {
     private final Name name;
     private final Vector<Type> templateTypes;
 
-    public ClassType(Name n) { this(new Token(),n,null); }
-    public ClassType(Token t, Name n) { this(t,n,null); }
+    public ClassType(Name n) { this(new Token(),n,new Vector<>()); }
+    public ClassType(Token t, Name n) { this(t,n,new Vector<>()); }
     public ClassType(Token t, Name n, Vector<Type> tt) {
         super(t);
         this.name = n;
