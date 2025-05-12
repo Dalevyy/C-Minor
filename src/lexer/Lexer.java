4c4e472398c2efaf9a3c9bc0bf990945b08cbc66
Dalev <dalevyy@gmail.com>
2025-05-11 18:46:25 -0700

    Updated formatting.

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -2,7 +2,6 @@ package lexer;
 
 import token.*;
 
-import java.util.Iterator;
 import utilities.Vector;
 import utilities.PrettyPrint;
 
@@ -10,12 +9,12 @@ public class Lexer {
 
     public static final char EOF = '\0'; // EOF
 
-    private final String file;           // User Program
-    private int currPos;                 // Position in file
-    private char lookChar;               // Current lookahead
-    private Location currLoc;            // Current location
-    private String currText;             // Current Text
-    private Vector<String> lines;        // User Program (as an array)
+    private final String file;              // User Program
+    private int currPos;                    // Position in file
+    private char lookChar;                  // Current lookahead
+    private final Location currLoc;         // Current location
+    private String currText;                // Current Text
+    private final Vector<String> lines;     // User Program (as an array)
 
     public Lexer(final String file) {
         this.file = file;
@@ -40,34 +39,24 @@ public class Lexer {
         start.column--;
         end.column--;
 
-        int strSize = 0;
+        int strSize;
         int startCol = start.column;
 
         for(int i = start.line; i <= end.line; i++) {
             String curr = lines.get(i);
 
-            if(i != end.line)
-                strSize = curr.length();
-            else
-                strSize = end.column;
+            if(i != end.line) { strSize = curr.length(); }
+            else { strSize = end.column; }
 
-            for(int j = startCol; j < strSize; j++)
-                sb.append(curr.charAt(j));
-
+            for(int j = startCol; j < strSize; j++) { sb.append(curr.charAt(j)); }
             startCol = 0;
         }
         return sb.toString();
     }
 
-    public void printSyntaxError(Position start) {
-        System.out.println(start.line + "| " + lines.get(start.line-1));
-    }
+    public void printSyntaxError(Position start) { System.out.println(start.line + "| " + lines.get(start.line-1)); }
 
-    public void debugPrint() {
-        Iterator<String> it = lines.iterator();
-        while(it.hasNext())
-            System.out.print(it.next());
-    }
+    public void debugPrint() { for (String line : lines) { System.out.print(line); } }
 
     private void consume() {
         currText += file.charAt(currPos);
@@ -76,30 +65,24 @@ public class Lexer {
             lines.get(currLoc.end.line-1);
             lines.set(currLoc.end.line-1,currText);
         }
-        catch(Exception e) {
-            lines.add(currLoc.end.line-1,currText);
-        }
+        catch(Exception e) { lines.add(currLoc.end.line-1,currText); }
 
         currPos += 1;
         currLoc.addCol();
 
-        if(currPos < file.length())
-            lookChar = file.charAt(currPos);
-        else
-            lookChar = EOF;
+        if(currPos < file.length()) { lookChar = file.charAt(currPos); }
+        else { lookChar = EOF; }
     }
 
     private boolean match(char expectedChar) {
-        if(lookChar != expectedChar)
-            return false;
+        if(lookChar != expectedChar) { return false; }
         consume();
         return true;
     }
 
     private void consumeWhitespace() {
         while(lookChar == ' ' || lookChar == '\t' || lookChar == '\r' || lookChar == '\n') {
-            if(lookChar == ' ' || lookChar == '\t')
-                consume();
+            if(lookChar == ' ' || lookChar == '\t') { consume(); }
             else {
                 consume();
                 currText = "";
@@ -110,14 +93,12 @@ public class Lexer {
     }
 
     private void consumeComment() {
-        while(lookChar != '\n')
-            consume();
+        while(lookChar != '\n') { consume(); }
         consumeWhitespace();
     }
 
     private void consumeMultiLineComment() {
-        while(!(match('*') && match('/')))
-            consume();
+        while(!(match('*') && match('/'))) { consume(); }
         consumeWhitespace();
     }
 
@@ -174,7 +155,8 @@ public class Lexer {
                 sb.append('\0');
                 break;
             default:
-                System.out.println(PrettyPrint.RED + "Error! Invalid escape sequence written at positions " + currLoc.toString() + ".");
+                System.out.println(PrettyPrint.RED + "Error! Invalid escape sequence written at positions "
+                                                   + currLoc.toString() + ".");
                 System.exit(1);
         }
     }
@@ -183,16 +165,13 @@ public class Lexer {
         StringBuilder newChar = new StringBuilder();
         newChar.append('\'');
 
-        if(match('\\')) {
-            escapeSequence(newChar);
-        }
+        if(match('\\')) { escapeSequence(newChar); }
         else {
             newChar.append(lookChar);
             consume();
         }
 
-        if(!match('\''))
-            return strLit(newChar);
+        if(!match('\'')) { return strLit(newChar); }
 
         newChar.append('\'');
         return new Token(TokenType.CHAR_LIT, newChar.toString(), currLoc.copy());
@@ -200,17 +179,16 @@ public class Lexer {
 
     private Token strLit(StringBuilder newStr) {
         while(!match('\'') && !isEOF()) {
-            if(match('\\')) {
-                escapeSequence(newStr);
-            }
+            if(match('\\')) { escapeSequence(newStr); }
             else {
                 newStr.append(lookChar);
                 consume();
             }
         }
 
-        if(match('\'') && match('\''))
+        if(match('\'') && match('\'')) {
             return new Token(TokenType.TEXT_LIT, newStr.toString(), currLoc.copy());
+        }
 
         newStr.append('\'');
         return new Token(TokenType.STR_LIT, newStr.toString(), currLoc.copy());
@@ -240,13 +218,13 @@ public class Lexer {
             consume();
         }
 
-        if(isDigit())
-            return createID(createStr);
+        if(isDigit()) { return createID(createStr); }
 
         String nextStr = createStr.toString();
 
-        if(nextStr.equals("as") && match('?'))
+        if(nextStr.equals("as") && match('?')) {
             return new Token(TokenType.AS, "as?", currLoc.copy());
+        }
 
         return switch(nextStr) {
             // -----------------------------------------------------------------------------------------------
@@ -331,7 +309,6 @@ public class Lexer {
 
             case "True" -> new Token(TokenType.BOOL_LIT, "True", currLoc.copy());
             case "False" -> new Token(TokenType.BOOL_LIT, "False", currLoc.copy());
-
             default -> createID(createStr);
         };
     }
@@ -347,9 +324,10 @@ public class Lexer {
                 newNum.append('.');
                 return realLit(newNum);
             }
-            else { currPos -= 1; }  // Sorta a Pedersen hack, need to do this to scan '..' correctly
+            // This is technically a Pedersen hack, but it's needed to make sure
+            // `..` is scanned correctly when a user writes a For Loop
+            else { currPos -= 1; }
         }
-
         return new Token(TokenType.INT_LIT, newNum.toString(), currLoc.copy());
     }
 
@@ -369,32 +347,24 @@ public class Lexer {
                     break;
                 case '=':
                     consume();
-                    if(match('='))
-                        return new Token(TokenType.EQEQ, "==", currLoc.copy());
-                    if(match('>'))
-                        return new Token(TokenType.ARROW, "=>", currLoc.copy());
+                    if(match('=')) { return new Token(TokenType.EQEQ, "==", currLoc.copy()); }
+                    if(match('>')) { return new Token(TokenType.ARROW, "=>", currLoc.copy()); }
                     return new Token(TokenType.EQ, "=", currLoc.copy());
                 case '+':
                     consume();
-                    if(match('='))
-                        return new Token(TokenType.PLUSEQ, "+=", currLoc.copy());
+                    if(match('=')) { return new Token(TokenType.PLUSEQ, "+=", currLoc.copy()); }
                     return new Token(TokenType.PLUS, "+", currLoc.copy());
                 case '-':
                     consume();
-                    if(match('='))
-                        return new Token(TokenType.MINUSEQ, "-=", currLoc.copy());
+                    if(match('=')) { return new Token(TokenType.MINUSEQ, "-=", currLoc.copy()); }
                     return new Token(TokenType.MINUS, "-", currLoc.copy());
                 case '*':
                     consume();
                     if(match('*')) {
-                        if(match('='))
-                            return new Token(TokenType.EXPEQ, "**=", currLoc.copy());
-
+                        if(match('=')) { return new Token(TokenType.EXPEQ, "**=", currLoc.copy()); }
                         return new Token(TokenType.EXP, "**", currLoc.copy());
                     }
-                    if(match('='))
-                        return new Token(TokenType.MULTEQ, "*=", currLoc.copy());
-
+                    if(match('=')) { return new Token(TokenType.MULTEQ, "*=", currLoc.copy()); }
                     return new Token(TokenType.MULT, "*", currLoc.copy());
                 case '/':
                     consume();
@@ -406,9 +376,7 @@ public class Lexer {
                         consumeMultiLineComment();
                         break;
                     }
-                    if(match('='))
-                        return new Token(TokenType.DIVEQ, "/=", currLoc.copy());
-
+                    if(match('=')) { return new Token(TokenType.DIVEQ, "/=", currLoc.copy()); }
                     return new Token(TokenType.DIV, "/", currLoc.copy());
                 case '~':
                     consume();
@@ -420,24 +388,21 @@ public class Lexer {
                     return new Token(TokenType.TILDE, "~", currLoc.copy());
                 case '%':
                     consume();
-                    if(match('='))
-                        return new Token(TokenType.MODEQ, "%=", currLoc.copy());
-
+                    if(match('=')) { return new Token(TokenType.MODEQ, "%=", currLoc.copy()); }
                     return new Token(TokenType.MOD, "%", currLoc.copy());
                 case '!':
                     consume();
-                    if(match('='))
-                        return new Token(TokenType.NEQ, "!=", currLoc.copy());
+                    if(match('=')) { return new Token(TokenType.NEQ, "!=", currLoc.copy()); }
                     if(isLetter()) {
                         StringBuilder sb = new StringBuilder();
                         while(isLetter()) {
                             sb.append(lookChar);
                             consume();
                         }
-                        if(sb.toString().equals("instanceof"))
+                        if(sb.toString().equals("instanceof")) {
                             return new Token(TokenType.NINSTANCEOF, "!instanceof", currLoc.copy());
+                        }
                     }
-                    // Default Error
                     return new Token(TokenType.ERROR, "ERROR", currLoc.copy());
                 case '#':
                     consume();
@@ -448,53 +413,37 @@ public class Lexer {
                             sb.append(lookChar);
                             consume();
                         }
-                        if(sb.toString().equals("include"))
+                        if(sb.toString().equals("include")) {
                             return new Token(TokenType.INCLUDE, "#include", currLoc.copy());
-                        if(sb.toString().equals("exclude"))
+                        }
+                        if(sb.toString().equals("exclude")) {
                             return new Token(TokenType.EXCLUDE, "#exclude", currLoc.copy());
-
+                        }
                     }
                     return new Token(TokenType.ERROR, "ERROR", currLoc.copy());
                 case '<':
                     consume();
                     if(match('=')) {
-                        if(match('>'))
-                            return new Token(TokenType.UFO, "<=>", currLoc.copy());
-
+                        if(match('>')) { return new Token(TokenType.UFO, "<=>", currLoc.copy()); }
                         return new Token(TokenType.LTEQ, "<=", currLoc.copy());
                     }
-
-                    if(match('>'))
-                        return new Token(TokenType.LTGT, "<>", currLoc.copy());
-
-                    if(match(':'))
-                        return new Token(TokenType.MIN, "<:", currLoc.copy());
-
-                    if(match('<'))
-                        return new Token(TokenType.SLEFT, "<<", currLoc.copy());
-
+                    if(match('>')) { return new Token(TokenType.LTGT, "<>", currLoc.copy()); }
+                    if(match(':')) { return new Token(TokenType.MIN, "<:", currLoc.copy()); }
+                    if(match('<')) { return new Token(TokenType.SLEFT, "<<", currLoc.copy()); }
                     return new Token(TokenType.LT, "<", currLoc.copy());
                 case '>':
                     consume();
-                    if(match('='))
-                        return new Token(TokenType.GTEQ, ">=", currLoc.copy());
-
-                    if(match('>'))
-                        return new Token(TokenType.SRIGHT, ">>", currLoc.copy());
-
+                    if(match('=')) { return new Token(TokenType.GTEQ, ">=", currLoc.copy()); }
+                    if(match('>')) { return new Token(TokenType.SRIGHT, ">>", currLoc.copy()); }
                     return new Token(TokenType.GT, ">", currLoc.copy());
                 case ':':
                     consume();
-                    if(match('>'))
-                        return new Token(TokenType.MAX, ":>", currLoc.copy());
+                    if(match('>')) { return new Token(TokenType.MAX, ":>", currLoc.copy()); }
                     return new Token(TokenType.COLON, ":", currLoc.copy());
                 case '.':
                     consume();
-                    if(isDigit())
-                        return realLit(new StringBuilder("."));
-                    if(match('.'))
-                        return new Token(TokenType.INC, "..", currLoc.copy());
-
+                    if(isDigit()) { return realLit(new StringBuilder(".")); }
+                    if(match('.')) { return new Token(TokenType.INC, "..", currLoc.copy()); }
                     return new Token(TokenType.PERIOD, ".", currLoc.copy());
                 case ',':
                     consume();
@@ -522,13 +471,11 @@ public class Lexer {
                     return new Token(TokenType.AT, "@", currLoc.copy());
                 case '\'':
                     consume();
-                    if(match('\'') && match('\''))
-                        return strLit(new StringBuilder());
+                    if(match('\'') && match('\'')) { return strLit(new StringBuilder()); }
                     return charLit();
                 case '?':
                     consume();
-                    if(match('.'))
-                        return new Token(TokenType.ELVIS, "?.", currLoc.copy());
+                    if(match('.')) { return new Token(TokenType.ELVIS, "?.", currLoc.copy()); }
                     else return new Token(TokenType.ERROR, "ERROR", currLoc.copy());
                 case '|':
                     consume();
@@ -540,11 +487,8 @@ public class Lexer {
                     consume();
                     return new Token(TokenType.XOR, "^", currLoc.copy());
                 default:
-                    if(isLetter() || lookChar == '_')
-                        return name();
-                    if(isDigit())
-                        return number(new StringBuilder());
-
+                    if(isLetter() || lookChar == '_') { return name(); }
+                    if(isDigit()) { return number(new StringBuilder()); }
                     consume();
                     return new Token(TokenType.ERROR, "ERROR", currLoc.copy());
             }
