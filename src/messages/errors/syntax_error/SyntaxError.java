bca5fc8e46fd583e2bd91da241e1d3434731a76d
Dalev <dalevyy@gmail.com>
2025-05-14 17:00:17 -0700

    Added header error message for syntax errors.

--- src/messages/errors/syntax_error/SyntaxError.java
+++ src/messages/errors/syntax_error/SyntaxError.java
@@ -1,10 +1,17 @@
 package messages.errors.syntax_error;
 
 import messages.errors.Error;
+import utilities.PrettyPrint;
 
 public class SyntaxError extends Error {
     public SyntaxError() {}
-    @Override
-    public String header() { return ""; }
 
+    @Override
+    public String header() {
+        if(fileName != null) {
+            return PrettyPrint.CYAN + "Syntax error detected in "
+                    + PrettyPrint.RESET + fileName() + "\n";
+        }
+        return PrettyPrint.CYAN + "Syntax error detected!" + PrettyPrint.RESET;
+    }
 }
