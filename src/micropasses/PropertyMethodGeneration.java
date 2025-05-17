8b6385f14e78cefb118d2cd761f112c8df7cc0ec
Dalev <dalevyy@gmail.com>
2025-05-17 00:21:54 -0700

    Refactored pass to work with AST builders.

--- src/micropasses/PropertyMethodGeneration.java
+++ src/micropasses/PropertyMethodGeneration.java
@@ -1,12 +1,18 @@
 package micropasses;
 
-import ast.*;
-import ast.expressions.FieldExpr;
+import ast.ParamDecl;
+import ast.ParamDecl.ParamDeclBuilder;
+import ast.expressions.FieldExpr.FieldExprBuilder;
 import ast.expressions.NameExpr;
+import ast.classbody.FieldDecl;
+import ast.classbody.MethodDecl;
+import ast.classbody.MethodDecl.MethodDeclBuilder;
+import ast.Modifier;
 import ast.Modifier.Mods;
-import ast.classbody.*;
-import ast.operators.AssignOp;
-import ast.statements.*;
+import ast.operators.AssignOp.AssignType;
+import ast.statements.AssignStmt.AssignStmtBuilder;
+import ast.statements.BlockStmt;
+import ast.statements.ReturnStmt.ReturnStmtBuilder;
 import ast.topleveldecls.ClassDecl;
 import ast.types.VoidType;
 import utilities.Vector;
@@ -21,57 +27,64 @@ import utilities.Visitor;
 */
 public class PropertyMethodGeneration extends Visitor {
 
-    // This method is responsible for creating a setter for the field
-    // A setter will just be a MethodDecl with an AssignStmt
     public MethodDecl createSetter(FieldDecl fd) {
-        Vector<Modifier> mods = new Vector<>();
-        mods.add(new Modifier(Mods.PUBLIC));
-        Name n = new Name("set_"+fd);
-        Vector<ParamDecl> param = new Vector<>();
-        param.add(new ParamDecl(new Modifier(Mods.IN),new Name("param" + fd),fd.type()));
-        BlockStmt b = new BlockStmt();
+        ParamDecl setterParam = new ParamDeclBuilder()
+                                        .setModifier(Mods.IN)
+                                        .setName("param"+fd)
+                                        .setType(fd.type())
+                                        .createParamDecl();
 
-        MethodDecl setter = new MethodDecl(mods,n,param,new VoidType(),b);
+        BlockStmt setterBlock = new BlockStmt();
+        setterBlock.addStmt(
+                new AssignStmtBuilder()
+                        .setLHS(
+                            new FieldExprBuilder()
+                                    .setTarget(new NameExpr("this"))
+                                    .setAccessExpr(new NameExpr(fd.toString()))
+                                    .createFieldExpr()
+                        )
+                        .setRHS(new NameExpr("param"+fd))
+                        .setAssignOp(AssignType.EQ)
+                        .createAssignStmt()
+        );
 
-        AssignStmt as = new AssignStmt(new FieldExpr(new NameExpr("this"), new NameExpr(fd.toString())),
-                                       new NameExpr("param" + fd),
-                                       new AssignOp(AssignOp.AssignType.EQ));
-
-        b.addStmt(as);
-
-        return setter;
+        return new MethodDeclBuilder()
+                        .setMods(new Vector<>(new Modifier(Mods.PUBLIC)))
+                        .setName("set_"+fd)
+                        .setParams(new Vector<>(setterParam))
+                        .setReturnType(new VoidType())
+                        .setBlockStmt(setterBlock)
+                        .createMethodDecl();
     }
 
-    // This method is responsible for creating a getter for the field.
-    // A getter will just be a MethodDecl with a ReturnStmt
     public MethodDecl createGetter(FieldDecl fd) {
-        Vector<Modifier> mods = new Vector<>();
-        mods.add(new Modifier(Mods.PUBLIC));
-        Name n = new Name("get_"+fd);
-        Vector<ParamDecl> param = new Vector<>();
+        BlockStmt b = new BlockStmt();
+        b.addStmt(
+                new ReturnStmtBuilder()
+                        .setReturnExpr(new FieldExprBuilder()
+                                .setTarget(new NameExpr("this"))
+                                .setAccessExpr(new NameExpr(fd.toString()))
+                                .createFieldExpr()
+                        )
+                        .createReturnStmt()
+        );
 
-        ReturnStmt rs = new ReturnStmt(new FieldExpr(new NameExpr("this"),
-                                                     new NameExpr(fd.toString())));
-        Vector<Statement> stmt = new Vector<>();
-        stmt.add(rs);
-
-        BlockStmt b = new BlockStmt(stmt);
-
-        MethodDecl getter = new MethodDecl(mods,n,param,fd.type(),b);
-
-        return getter;
+        return new MethodDeclBuilder()
+                        .setMods(new Vector<>(new Modifier(Mods.PUBLIC)))
+                        .setName("get_"+fd)
+                        .setParams(new Vector<>())
+                        .setReturnType(fd.type())
+                        .setBlockStmt(b)
+                        .createMethodDecl();
     }
 
     public void visitClassDecl(ClassDecl cd) {
         Vector<FieldDecl> fields = cd.classBlock().fieldDecls();
 
-        for(int i = 0; i < fields.size(); i++) {
-            FieldDecl currField = fields.get(i);
-            if(currField.mod.isProperty()) {
-                // First, create the setter method
-                cd.classBlock().methodDecls().add(0,createSetter(currField));
-                // Then, create the getter method
-                cd.classBlock().methodDecls().add(0,createGetter(currField));
+        for(FieldDecl fd : cd.classBlock().fieldDecls()) {
+            if(fd.mod.isProperty()) {
+                cd.classBlock().methodDecls().add(createSetter(fd));
+                cd.classBlock().methodDecls().add(createGetter(fd));
             }
         }
     }
