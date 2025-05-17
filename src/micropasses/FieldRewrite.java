28a2fcb25a99f407c579dfc13fce15719aefdfd0
Dalev <dalevyy@gmail.com>
2025-05-17 00:50:04 -0700

    Added javadoc.

--- src/micropasses/FieldRewrite.java
+++ src/micropasses/FieldRewrite.java
@@ -7,6 +7,19 @@ import ast.topleveldecls.ClassDecl;
 import utilities.SymbolTable;
 import utilities.Visitor;
 
+/**
+ * Micropass #4
+ * <br><br>
+ * Once name checking is complete, we want to go back through each class
+ * in a C Minor program and make sure all <code>NameExpr</code> nodes that
+ * represent fields are rewritten to be <code>FieldExpr</code> nodes instead.
+ * This is needed as we have to internally keep track of whether or not the
+ * <code>NameExpr</code> refers to a field since during execution, we have to
+ * be able to evaluate the value of the field based on the current object. This
+ * will be done by setting the target to be <code>this</code> when we generate
+ * the replacement <code>FieldExpr</code> node.
+ * @author Daniel Levy
+ */
 public class FieldRewrite extends Visitor {
 
     private SymbolTable currentScope;
