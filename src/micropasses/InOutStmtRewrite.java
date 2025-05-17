398010bea7dc8dd95132a54e4e849113b7b8f6d2
Dalev <dalevyy@gmail.com>
2025-05-16 23:08:42 -0700

    Added javadoc.

--- src/micropasses/InOutStmtRewrite.java
+++ src/micropasses/InOutStmtRewrite.java
@@ -1,26 +1,29 @@
 package micropasses;
 
-import ast.expressions.*;
+import ast.expressions.BinaryExpr;
+import ast.expressions.Expression;
+import ast.expressions.InStmt;
+import ast.expressions.OutStmt;
 import messages.errors.ErrorBuilder;
 import messages.errors.scope_error.ScopeErrorFactory;
 import messages.MessageType;
 import utilities.Vector;
 import utilities.Visitor;
 
-/*
-    Micropass #1: OutStmt and InStmt Rewriting
-
-    This micro pass will rewrite any OutStmt and InStmt
-    nodes in the current AST. The parser will not generate
-    the correct AST nodes since it will parse these expressions
-    as if they were binary expressions.
-
-    This pass is designed to take all binary expressions written
-    with '<<' and '>>' in an OutStmt/InStmt and change them into
-    individual expressions, so we can individually print out everything.
-
-*/
-
+/**
+ * Micropass #1
+ * <br><br>
+ * Due to the nature of the C Minor grammar, <code>InStmt</code> and <code>OutStmt</code> nodes
+ * within the <code>AST</code> will not be written correctly. This is because the parser will
+ * treat the <code>'<<'</code> and <code>'>>'</code> operators as binary shift operators instead
+ * of the stream insertion and extraction operators. Thus, after we finish parsing, we need to
+ * do a pass to rewrite these statements to ensure the <code>AST</code> is correctly written.
+ * <br><br>
+ * Additionally, for <code>InStmt</code> nodes, we will also check to make sure each expression
+ * in the <code>InStmt</code> represents a <code>NameExpr</code> since data needs to be stored
+ * in a memory location that the name points to.
+ * @author Daniel Levy
+ */
 public class InOutStmtRewrite extends Visitor {
 
     private boolean insideIO = false;
