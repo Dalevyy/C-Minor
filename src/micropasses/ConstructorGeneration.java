1c9fb84bace749212287762bc413be4b9adee986
Dalev <dalevyy@gmail.com>
2025-05-17 01:19:31 -0700

    Refactored micropass.

--- src/micropasses/ConstructorGeneration.java
+++ src/micropasses/ConstructorGeneration.java
@@ -1,37 +1,37 @@
 package micropasses;
 
-import ast.*;
 import ast.classbody.*;
+import ast.expressions.FieldExpr.FieldExprBuilder;
 import ast.expressions.NameExpr;
-import ast.operators.AssignOp;
-import ast.topleveldecls.*;
+import ast.operators.AssignOp.AssignType;
+import ast.topleveldecls.ClassDecl;
 import ast.statements.AssignStmt;
-import token.*;
-import utilities.*;
-import java.util.HashMap;
+import ast.statements.AssignStmt.AssignStmtBuilder;
+import utilities.Vector;
+import utilities.Visitor;
 
-/*
-MICROPASS #2 : Constructor Generation after type checking
-*/
 public class ConstructorGeneration extends Visitor {
 
     public void visitClassDecl(ClassDecl cd) {
-        SymbolTable declNames = cd.symbolTable;
-        HashMap<String, NameNode> fields = declNames.getAllNames();
+        Vector<AssignStmt> initParams = new Vector<>();
 
-        Vector<AssignStmt> initParams = new Vector<AssignStmt>();
-
-        for(String key : fields.keySet()) {
-            if(fields.get(key).decl().isFieldDecl()) {
-                FieldDecl currFieldDecl = fields.get(key).decl().asFieldDecl();
-                initParams.add( new AssignStmt(new Token(null,"",new Location()),new NameExpr(new Token(null,"",new Location()),currFieldDecl.var().name()),currFieldDecl.var().init(),new AssignOp(new Token(null,"",new Location()), AssignOp.AssignType.EQ)));
+        for(String key : cd.symbolTable.getAllNames().keySet()) {
+            if(cd.symbolTable.findName(key).decl().isFieldDecl()) {
+                FieldDecl currFieldDecl = cd.symbolTable.findName(key).decl().asFieldDecl();
+                initParams.add(
+                    new AssignStmtBuilder()
+                            .setLHS(
+                                new FieldExprBuilder()
+                                        .setTarget(new NameExpr("this"))
+                                        .setAccessExpr(new NameExpr(currFieldDecl.toString()))
+                                        .createFieldExpr()
+                            )
+                            .setRHS(currFieldDecl.var().init())
+                            .setAssignOp(AssignType.EQ)
+                            .createAssignStmt()
+                );
             }
         }
-
-        // A C Minor constructor will not directly be part of the AST,
-        // so only a ClassDecl may access its constructor
-        InitDecl newInit = new InitDecl(initParams);
-        cd.setConstructor(newInit);
+        cd.setConstructor(new InitDecl(initParams));
     }
-
 }
