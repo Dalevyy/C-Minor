ae5e0f307ce291c4bd8e8a472aaa3e43df382105
Dalev <dalevyy@gmail.com>
2025-05-17 01:22:26 -0700

    Added javadoc.

--- src/micropasses/ConstructorGeneration.java
+++ src/micropasses/ConstructorGeneration.java
@@ -10,6 +10,17 @@ import ast.statements.AssignStmt.AssignStmtBuilder;
 import utilities.Vector;
 import utilities.Visitor;
 
+/**
+ * Micropass #6
+ * <br><br>
+ * In C Minor, we do not allow a user to create class constructors. Instead, we
+ * will provide a constructor for the user, and this pass will generate a constructor
+ * after type checking is completed. In C Minor, a constructor is represented by an
+ * <code>InitDecl</code> node and is not directly part of an <code>AST</code>. The
+ * constructor will also only contain <code>AssignStmt</code> nodes.
+ *
+ * @author Daniel Levy
+ */
 public class ConstructorGeneration extends Visitor {
 
     public void visitClassDecl(ClassDecl cd) {
