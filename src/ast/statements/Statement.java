7c09a4a8eaee1ba49a181313bf67ed21d500bcb6
Dalev <dalevyy@gmail.com>
2025-05-23 17:32:06 -0700

    Changed order of method declarations.

--- src/ast/statements/Statement.java
+++ src/ast/statements/Statement.java
@@ -14,36 +14,39 @@ public abstract class Statement extends AST {
     public boolean isAssignStmt() { return false; }
     public AssignStmt asAssignStmt() { throw new RuntimeException("Expression can not be casted into an AssignStmt.\n"); }
 
-    public boolean isExprStmt() { return false; }
-    public ExprStmt asExprStmt() { throw new RuntimeException("Expression can not be casted into an ExprStmt.\n"); }
-
     public boolean isBlockStmt() { return false; }
     public BlockStmt asBlockStmt() { throw new RuntimeException("Expression can not be casted into a BlockStmt.\n"); }
-
-    public boolean isStopStmt() { return false; }
-    public StopStmt asStopStmt() { throw new RuntimeException("Expression can not be casted into a StopStmt.\n"); }
-
-    public boolean isReturnStmt() { return false; }
-    public ReturnStmt asReturnStmt() { throw new RuntimeException("Expression can not be casted into a ReturnStmt.\n"); }
-
-    public boolean isIfStmt() { return false; }
-    public IfStmt asIfStmt() { throw new RuntimeException("Expression can not be casted into an IfStmt.\n"); }
-
-    public boolean isWhileStmt() { return false; }
-    public WhileStmt asWhileStmt() { throw new RuntimeException("Expression can not be casted into a WhileStmt.\n"); }
-
-    public boolean isDoStmt() { return false; }
-    public DoStmt asDoStmt() { throw new RuntimeException("Expression can not be casted into a DoStmt.\n"); }
-
-    public boolean isForStmt() { return false; }
-    public ForStmt asForStmt() { throw new RuntimeException("Expression can not be casted into a ForStmt.\n"); }
-
-    public boolean isChoiceStmt() { return false; }
-    public ChoiceStmt asChoiceStmt() { throw new RuntimeException("Expression can not be casted into a ChoiceStmt.\n"); }
 
     public boolean isCaseStmt() { return false; }
     public CaseStmt asCaseStmt() { throw new RuntimeException("Expression can not be casted into a CaseStmt.\n"); }
 
+    public boolean isChoiceStmt() { return false; }
+    public ChoiceStmt asChoiceStmt() { throw new RuntimeException("Expression can not be casted into a ChoiceStmt.\n"); }
+
+    public boolean isDoStmt() { return false; }
+    public DoStmt asDoStmt() { throw new RuntimeException("Expression can not be casted into a DoStmt.\n"); }
+
+    public boolean isExprStmt() { return false; }
+    public ExprStmt asExprStmt() { throw new RuntimeException("Expression can not be casted into an ExprStmt.\n"); }
+
+    public boolean isForStmt() { return false; }
+    public ForStmt asForStmt() { throw new RuntimeException("Expression can not be casted into a ForStmt.\n"); }
+
+    public boolean isIfStmt() { return false; }
+    public IfStmt asIfStmt() { throw new RuntimeException("Expression can not be casted into an IfStmt.\n"); }
+
     public boolean isLocalDecl() { return false; }
     public LocalDecl asLocalDecl() { throw new RuntimeException("Expression can not be casted into a LocalDecl.\n"); }
+
+    public boolean isReturnStmt() { return false; }
+    public ReturnStmt asReturnStmt() { throw new RuntimeException("Expression can not be casted into a ReturnStmt.\n"); }
+
+    public boolean isRetypeStmt() { return false; }
+    public RetypeStmt asRetypeStmt() { throw new RuntimeException("Expression can not be casted into a RetypeStmt\n"); }
+
+    public boolean isStopStmt() { return false; }
+    public StopStmt asStopStmt() { throw new RuntimeException("Expression can not be casted into a StopStmt.\n"); }
+
+    public boolean isWhileStmt() { return false; }
+    public WhileStmt asWhileStmt() { throw new RuntimeException("Expression can not be casted into a WhileStmt.\n"); }
 }
