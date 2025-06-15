e57b0139d5f7913a8f613aa989d4ebbf4dffbf36
Dalev <dalevyy@gmail.com>
2025-06-15 11:18:41 -0700

    Readjusted micropass.

--- src/micropasses/InOutStmtRewrite.java
+++ src/micropasses/InOutStmtRewrite.java
@@ -1,14 +1,11 @@
 package micropasses;
 
-import ast.expressions.BinaryExpr;
 import ast.expressions.Expression;
-import ast.expressions.FieldExpr;
 import ast.expressions.InStmt;
 import ast.expressions.OutStmt;
-import messages.errors.ErrorBuilder;
 import messages.errors.scope.ScopeErrorFactory;
+import messages.errors.ErrorBuilder;
 import messages.MessageType;
-import utilities.Vector;
 import utilities.Visitor;
 
 /**
@@ -28,10 +25,8 @@ import utilities.Visitor;
  * @author Daniel Levy
  */
 public class InOutStmtRewrite extends Visitor {
-
-    private boolean insideIO = false;
+    
     private final ScopeErrorFactory generateScopeError;
-    private Vector<Expression> ioExprs;
 
     public InOutStmtRewrite() { generateScopeError = new ScopeErrorFactory(); }
     public InOutStmtRewrite(boolean interpretMode) {
@@ -39,93 +34,26 @@ public class InOutStmtRewrite extends Visitor {
         this.interpretMode = interpretMode;
     }
 
-    public void visitBinaryExpr(BinaryExpr be) {
-        if(insideIO) {
-            switch(be.binaryOp().toString()) {
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-                case "%":
-                case "**":
-                    ioExprs.add(be);
-                    break;
-                case "<<":
-                case ">>":
-                    if(!be.toString().startsWith("(")) {
-                        be.LHS().visit(this);
-                        if(!be.LHS().isBinaryExpr())
-                            ioExprs.add(be.LHS());
-                    }
-                    else
-                        ioExprs.add(be.LHS());
-
-                    if(be.RHS().isBinaryExpr())
-                        be.RHS().visit(this);
-                    else
-                        ioExprs.add(be.RHS());
-                    break;
-                default:
-                    ioExprs.add(be);
-                    if(be.RHS().isFieldExpr()) { be.RHS().visit(this); }
-            }
-        }
-    }
-
-    public void visitFieldExpr(FieldExpr fe) {
-       if(insideIO) {
-            if(fe.accessExpr().isBinaryExpr()) {
-                ioExprs.add(fe.accessExpr().asBinaryExpr().RHS());
-                fe.setFieldAccess(fe.accessExpr().asBinaryExpr().LHS());
-            }
-            else { super.visitFieldExpr(fe); }
+    public void visitInStmt(InStmt is) {
+        // ERROR CHECK #1: Each expression in an InStmt has to be a name or else
+        //                 we can not store any input values from the user
+        for(Expression e : is.inExprs()) {
+            if(!e.isNameExpr())
+                new ErrorBuilder(generateScopeError,this.interpretMode)
+                    .addLocation(is)
+                    .addErrorType(MessageType.SCOPE_ERROR_327)
+                    .error();
         }
     }
 
     public void visitOutStmt(OutStmt os) {
-        insideIO = true;
-        ioExprs = new Vector<>();
-
-        if(os.outExprs().get(0).isFieldExpr()) {
-            os.removeChild(0);
-            ioExprs.add(os.outExprs().get(0));
-            os.outExprs().get(0).visit(this);
-            os.setOutExprs(ioExprs);
-            os.addChild(ioExprs);
-        }
-        else if(os.outExprs().get(0).isBinaryExpr()) {
-            os.removeChild(0);
-            os.outExprs().get(0).visit(this);
-            os.setOutExprs(ioExprs);
-            os.addChild(ioExprs);
-        }
-
-        insideIO = false;
-    }
-
-    public void visitInStmt(InStmt in) {
-        insideIO = true;
-        ioExprs = new Vector<>();
-        
-        if(in.inExprs().get(0).isBinaryExpr()) {
-            in.removeChild(0);
-            for(Expression e : in.inExprs()) { e.visit(this); }
-
-            in.setInExprs(ioExprs);
-            in.addChild(ioExprs);
-        }
-
-        // ERROR CHECK #1: Each expression in an InStmt has to be a name or else
-        //                 we can not store any input values from the user
-        for(Expression e : in.inExprs()) {
-            if(!e.isNameExpr()) {
+        // ERROR CHECK #1: Make sure the output statement does not have an input statement
+        for(Expression e : os.outExprs()) {
+            if(e.isInStmt())
                 new ErrorBuilder(generateScopeError,this.interpretMode)
-                        .addLocation(in)
-                        .addErrorType(MessageType.SCOPE_ERROR_327)
-                        .error();
-            }
+                    .addLocation(os)
+                    .addErrorType(MessageType.SYNTAX_ERROR_101)
+                    .error();
         }
-        
-        insideIO = false;
     }
 }
