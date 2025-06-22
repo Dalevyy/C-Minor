705684ad1394026e7237c2e166bbf6f9d34b519b
Dalev <dalevyy@gmail.com>
2025-06-21 18:22:58 -0700

    Refactored class to work with inherited classes better.

--- src/messages/errors/Error.java
+++ src/messages/errors/Error.java
@@ -1,5 +1,11 @@
 package messages.errors;
 
+import messages.errors.mod.ModError;
+import messages.errors.runtime.RuntimeError;
+import messages.errors.scope.ScopeError;
+import messages.errors.semantic.SemanticError;
+import messages.errors.syntax.SyntaxError;
+import messages.errors.type.TypeError;
 import messages.Message;
 import messages.MessageType;
 import utilities.PrettyPrint;
@@ -18,15 +24,19 @@ public abstract class Error extends Message {
     public String createMessage() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append(this.header());
-        if(this.location() != null)
-            sb.append(this.location().line());
-        sb.append(this.buildError());
-        if(this.suggest != null) {
-            sb.append("\n");
-            sb.append(this.buildSuggestion());
-        }
-        System.out.println(sb);
+        sb.append(header());
+        if(location != null)
+            sb.append(location.line());
+
+        sb.append(buildError());
+        if(suggest != null)
+            sb.append("\n").append(buildSuggestion());
+
+        return sb.toString();
+    }
+
+    public String printMessage() {
+        System.out.println(createMessage());
 
         if(!interpretMode)
             System.exit(1);
@@ -44,7 +54,7 @@ public abstract class Error extends Message {
         else
             throw new RuntimeException("Error");
         
-        return sb.toString();
+        return "";
     }
 
     private String buildError() {
@@ -69,9 +79,28 @@ public abstract class Error extends Message {
         return suggestMsg;
     }
 
-    public void setErrorType(MessageType et) { this.error = et; }
-    public MessageType errorType() { return this.error; }
+    public void setErrorType(MessageType et) { error = et; }
+    public MessageType errorType() { return error; }
 
-    public void setSuggestType(MessageType et) { this.suggest = et; }
-    public MessageType suggestType() { return this.suggest; }
+    public void setSuggestType(MessageType et) { suggest = et; }
+    public MessageType suggestType() { return suggest; }
+
+    // Helper Methods
+    public boolean isModifierError() { return false; }
+    public ModError asModifierError() { throw new RuntimeException("The following error does not represent a modifier error"); }
+
+    public boolean isRuntimeError() { return false; }
+    public RuntimeError asRuntimeError() { throw new RuntimeException("The following error does not represent a runtime error"); }
+
+    public boolean isScopeError() { return false; }
+    public ScopeError asScopeError() { throw new RuntimeException("The following error does not represent a scope error"); }
+
+    public boolean isSemanticError() { return false; }
+    public SemanticError asSemanticError() { throw new RuntimeException("The following error does not represent a semantic error"); }
+
+    public boolean isSyntaxError() { return false; }
+    public SyntaxError asSyntaxError() { throw new RuntimeException("The following error does not represent a syntax error"); }
+
+    public boolean isTypeError() { return false; }
+    public TypeError asTypeError() { throw new RuntimeException("The following error does not represent a type error"); }
 }
