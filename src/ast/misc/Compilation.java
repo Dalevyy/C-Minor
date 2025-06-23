1add5474b7334f95ae06445131d14f698d96c122
Dalev <dalevyy@gmail.com>
2025-06-23 10:39:05 -0700

    Added additional options for helper methods.

--- src/ast/misc/Compilation.java
+++ src/ast/misc/Compilation.java
@@ -17,6 +17,7 @@ public class Compilation extends AST {
 
     public SymbolTable globalTable;
 
+    private final Vector<Import> imports;
     private final Vector<EnumDecl> enumDecls;
     private final Vector<GlobalDecl> globalDecls;
     private final Vector<ClassDecl> classDecls;
@@ -25,6 +26,7 @@ public class Compilation extends AST {
 
     public Compilation() {
         this.globalTable = new SymbolTable();
+        this.imports = new Vector<>();
         this.enumDecls = new Vector<>();
         this.globalDecls = new Vector<>();
         this.classDecls = new Vector<>();
@@ -32,15 +34,17 @@ public class Compilation extends AST {
         this.mainFunc = new MainDecl();
     }
 
-    public Compilation(Token t, Vector<EnumDecl> ed, Vector<GlobalDecl> gd,
+    public Compilation(Token t, Vector<Import> ims, Vector<EnumDecl> ed, Vector<GlobalDecl> gd,
                        Vector<ClassDecl> cd, Vector<FuncDecl> fd, MainDecl m) {
         super(t);
+        imports = ims;
         enumDecls = ed;
         globalDecls = gd;
         classDecls = cd;
         funcDecls = fd;
         mainFunc = m;
 
+        addChild(imports);
         addChild(enumDecls);
         addChild(globalDecls);
         addChild(classDecls);
@@ -55,10 +59,25 @@ public class Compilation extends AST {
     public Vector<FuncDecl> funcDecls() { return funcDecls; }
     public MainDecl mainDecl() { return mainFunc; }
 
-    public void addEnumDecl(EnumDecl ed) { this.enumDecls.add(ed); }
-    public void addGlobalDecl(Vector<GlobalDecl> gd) { this.globalDecls.merge(gd); }
-    public void addClassDecl(ClassDecl cd) { this.classDecls.add(cd); }
-    public void addFuncDecl(FuncDecl fd) { this.funcDecls.add(fd); }
+    public void mergeCompilationUnit(Compilation c) {
+        globalTable.setParent(c.globalTable);
+        this.addEnumDecl(c.enumDecls);
+        this.addGlobalDecl(c.globalDecls);
+        this.addClassDecl(c.classDecls);
+        this.addFuncDecl(c.funcDecls);
+    }
+
+    public void addEnumDecl(EnumDecl ed) { enumDecls.add(ed); }
+    public void addEnumDecl(Vector<EnumDecl> ed) { enumDecls.addAll(ed); }
+
+    public void addGlobalDecl(GlobalDecl gd) { globalDecls.add(gd); }
+    public void addGlobalDecl(Vector<GlobalDecl> gd) { globalDecls.addAll(gd); }
+
+    public void addClassDecl(ClassDecl cd) { classDecls.add(cd); }
+    public void addClassDecl(Vector<ClassDecl> cd) { classDecls.addAll(cd); }
+
+    public void addFuncDecl(FuncDecl fd) { funcDecls.add(fd); }
+    public void addFuncDecl(Vector<FuncDecl> fd) { funcDecls.addAll(fd); }
 
     public boolean isCompilation() { return true; }
     public Compilation asCompilation() { return this; }
