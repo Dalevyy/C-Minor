ea22317de97c3f76ac97b454bcacd9f70e117e2a
Dalev <dalevyy@gmail.com>
2025-06-24 13:34:20 -0700

    Changed class name to `ImportDecl`.

--- src/ast/topleveldecls/ImportDecl.java
+++ src/ast/topleveldecls/ImportDecl.java
@@ -3,29 +3,28 @@ package ast.topleveldecls;
 import ast.misc.Compilation;
 import ast.misc.Name;
 import token.Token;
-import utilities.Vector;
 import utilities.Visitor;
 
-public class Import extends TopLevelDecl{
+public class ImportDecl extends TopLevelDecl{
 
     public Name fileName;
-    public Vector<Compilation> compiledFiles;
+    private Compilation compilationUnit;
 
-    public Import(Token metaData, Name n) {
+    public ImportDecl(Token metaData, Name n) {
         super(metaData);
         // Removing start/end quotes from file name for cleaner error outputting.
         n.setName(n.toString().substring(1,n.toString().length()-1));
         fileName = n;
-        compiledFiles = new Vector<>();
 
         addChild(fileName);
         setParent();
     }
 
     public boolean isImport() { return true; }
-    public Import asImport() { return this; }
+    public ImportDecl asImport() { return this; }
 
-    public void addCompilationUnit(Compilation c) { compiledFiles.add(c); }
+    public void addCompilationUnit(Compilation c) { this.compilationUnit = c; }
+    public Compilation getCompilationUnit() { return this.compilationUnit; }
 
     @Override
     public String toString() { return fileName.toString(); }
