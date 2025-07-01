20ae141bbd2657b15a210405f079ec9a0754032e
Dalev <dalevyy@gmail.com>
2025-07-01 12:09:02 -0700

    Updated class to work with new AST builders.

--- src/micropasses/PropertyMethodGeneration.java
+++ src/micropasses/PropertyMethodGeneration.java
@@ -4,10 +4,13 @@ import ast.classbody.FieldDecl;
 import ast.classbody.MethodDecl;
 import ast.classbody.MethodDecl.MethodDeclBuilder;
 import ast.expressions.FieldExpr.FieldExprBuilder;
-import ast.expressions.NameExpr;
+import ast.expressions.NameExpr.NameExprBuilder;
 import ast.expressions.ThisStmt;
 import ast.misc.Modifier;
 import ast.misc.Modifier.Mods;
+import ast.misc.Modifiers;
+import ast.misc.Name;
+import ast.operators.AssignOp;
 import ast.operators.AssignOp.AssignType;
 import ast.misc.ParamDecl.ParamDeclBuilder;
 import ast.statements.AssignStmt.AssignStmtBuilder;
@@ -19,78 +22,111 @@ import utilities.Vector;
 import utilities.Visitor;
 
 /**
- * Micropass #2
- * <br><br>
- * Before we start name checking, we need to generate <code>MethodDecl</code> nodes
- * for all fields declared with the <code>property</code> modifier. ThisStmt automatically
- * generates a setter,<code> set_x</code>, and a getter, <code>get_x</code> for a field
- * <i>x</i>. We need to make sure this is done prior to name checking or else we will
- * produce incorrect scope errors.
+ * A {@link Visitor} class that generates getters and setters for class fields.
+ * <p><br>
+ *     C Minor supports the {@code property} attribute which allows a user to
+ *     automatically generate a getter and setter for a field. The getter and
+ *     setter will be named {@code get_<fieldName>} and {@code set_<fieldName>}
+ *     respectively. This will be done prior to name checking to prevent incorrect
+ *     scope errors.
+ * </p>
  * @author Daniel Levy
  */
 public class PropertyMethodGeneration extends Visitor {
-//
-//    public MethodDecl createSetter(FieldDecl fd) {
-//        return new MethodDeclBuilder()
-//                .setMods(new Vector<>(new Modifier(Mods.PUBLIC)))
-//                .setMethodName("set_"+fd)
-//                .setParams(new Vector<>(
-//                    new ParamDeclBuilder()
-//                        .setModifier(Mods.IN)
-//                        .setName("param"+fd)
-//                        .setType(fd.type())
-//                        .createParamDecl())
-//                )
-//                .setReturnType(new VoidType())
-//                .setBlockStmt(
-//                    new BlockStmtBuilder()
-//                        .setStmts(
-//                            new AssignStmtBuilder()
-//                                .setLHS(
-//                                    new FieldExprBuilder()
-//                                            .setTarget(new ThisStmt())
-//                                            .setAccessExpr(new NameExpr(fd.toString()))
-//                                            .create()
-//                                )
-//                                .setRHS(new NameExpr("param"+fd))
-//                                .setAssignOp(AssignType.EQ)
-//                                .createAssignStmt()
-//                        )
-//                        .createBlockStmt()
-//                )
-//                .createMethodDecl();
-//    }
-//
-//    public MethodDecl createGetter(FieldDecl fd) {
-//        return new MethodDeclBuilder()
-//                .setMods(new Vector<>(new Modifier(Mods.PUBLIC)))
-//                .setMethodName("get_"+fd)
-//                .setParams(new Vector<>())
-//                .setReturnType(fd.type())
-//                .setBlockStmt(
-//                    new BlockStmtBuilder()
-//                        .setStmts(
-//                            new ReturnStmtBuilder()
-//                                .setReturnExpr(new FieldExprBuilder()
-//                                        .setTarget(new ThisStmt())
-//                                        .setAccessExpr(new NameExpr(fd.toString()))
-//                                        .create()
-//                                )
-//                                .create()
-//                        )
-//                        .createBlockStmt()
-//                )
-//                .createMethodDecl();
-//    }
 
-//    public void visitClassDecl(ClassDecl cd) {
-//        Vector<FieldDecl> fields = cd.classBlock().getFields();
-//
-//        for(FieldDecl fd : cd.classBlock().getFields()) {
-//            if(fd.mod.isProperty()) {
-//                cd.classBlock().getMethods().add(createSetter(fd));
-//                cd.classBlock().getMethods().add(createGetter(fd));
-//            }
-//        }
-//    }
+    /**
+     * Generates a setter method for the current field.
+     * @param fd Field Declaration
+     * @return Method Declaration
+     */
+    public MethodDecl createSetter(FieldDecl fd) {
+        return new MethodDeclBuilder()
+                   .setMods(new Modifiers(new Modifier(Mods.PUBLIC)))
+                   .setMethodName(new Name("set_" + fd))
+                   .setParams(
+                       new Vector<>(
+                           new ParamDeclBuilder()
+                               .setMod(new Modifiers(new Modifier(Mods.IN)))
+                               .setName(new Name("param" + fd))
+                               .setType(fd.type())
+                               .create()
+                       )
+                   )
+                   .setReturnType(new VoidType())
+                   .setBlockStmt(
+                        new BlockStmtBuilder()
+                            .setStmts(
+                                new Vector<>(
+                                    new AssignStmtBuilder()
+                                        .setLHS(
+                                            new FieldExprBuilder()
+                                                .setTarget(new ThisStmt())
+                                                .setAccessExpr(
+                                                    new NameExprBuilder()
+                                                        .setName(new Name(fd.toString()))
+                                                        .create()
+                                                )
+                                                .create()
+                                        )
+                                        .setRHS(
+                                            new NameExprBuilder()
+                                                .setName(new Name("param" + fd))
+                                                .create()
+                                        )
+                                        .setAssignOp(new AssignOp(AssignType.EQ))
+                                        .create()
+                                )
+                            )
+                            .create()
+                   )
+                   .create();
+    }
+
+    /**
+     * Generates a getter method for the current field.
+     * @param fd Field Declaration
+     * @return Method Declaration
+     */
+    public MethodDecl createGetter(FieldDecl fd) {
+        return new MethodDeclBuilder()
+                   .setMods(new Modifiers(new Modifier(Mods.PUBLIC)))
+                   .setMethodName(new Name("get_" + fd))
+                   .setReturnType(fd.type())
+                   .setBlockStmt(
+                       new BlockStmtBuilder()
+                           .setStmts(
+                               new Vector<>(
+                                   new ReturnStmtBuilder()
+                                       .setReturnExpr(
+                                           new FieldExprBuilder()
+                                               .setTarget(new ThisStmt())
+                                               .setAccessExpr(
+                                                   new NameExprBuilder()
+                                                       .setName(new Name(fd.toString()))
+                                                       .create()
+                                               )
+                                               .create()
+                                       )
+                                       .create()
+                               )
+                           )
+                           .create()
+                   )
+                   .create();
+    }
+
+    /**
+     * Generates a getter and setter for each field marked as {@code property} in the current class.
+     * @param cd Class Declaration
+     */
+    public void visitClassDecl(ClassDecl cd) {
+        Vector<FieldDecl> fields = cd.classBlock().getFields();
+
+        for(FieldDecl fd : cd.classBlock().getFields()) {
+            if(fd.mod.isProperty()) {
+                cd.classBlock().getMethods().add(createSetter(fd));
+                cd.classBlock().getMethods().add(createGetter(fd));
+            }
+        }
+    }
 }
