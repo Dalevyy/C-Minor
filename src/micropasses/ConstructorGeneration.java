3a5414c89eaae2360ea9671e8c4a412c8fcccd01
Dalev <dalevyy@gmail.com>
2025-07-01 11:38:28 -0700

    Refactored class to work with new AST builders.

--- src/micropasses/ConstructorGeneration.java
+++ src/micropasses/ConstructorGeneration.java
@@ -1,10 +1,11 @@
 package micropasses;
 
 import ast.classbody.FieldDecl;
-import ast.classbody.InitDecl;
+import ast.classbody.InitDecl.InitDeclBuilder;
 import ast.expressions.FieldExpr.FieldExprBuilder;
-import ast.expressions.NameExpr;
-import ast.operators.AssignOp;
+import ast.expressions.NameExpr.NameExprBuilder;
+import ast.expressions.ThisStmt;
+import ast.operators.AssignOp.AssignOpBuilder;
 import ast.operators.AssignOp.AssignType;
 import ast.topleveldecls.ClassDecl;
 import ast.statements.AssignStmt;
@@ -13,41 +14,57 @@ import utilities.Vector;
 import utilities.Visitor;
 
 /**
- * Micropass #6
- * <br><br>
- * In C Minor, we do not allow a user to create class constructors. Instead, we
- * will provide a constructor for the user, and this pass will generate a constructor
- * after type checking is completed. In C Minor, a constructor is represented by an
- * <code>InitDecl</code> node and is not directly part of an <code>AST</code>. The
- * constructor will also only contain <code>AssignStmt</code> nodes.
- *
+ * A {@link Visitor} class designed to generate class constructors.
+ * <p><br>
+ *     C Minor does not require (nor allow) a programmer to create constructors
+ *     for their classes. Instead, the compiler will generate a single constructor
+ *     for each class. This will be represented as an {@link ast.classbody.InitDecl}
+ *     internally. A C Minor constructor will just be a sequence of {@link AssignStmt}s
+ *     that will initialize every field to a class to a default value unless the user
+ *     specified a different initial value within a {@link ast.expressions.NewExpr}
+ * </p>
  * @author Daniel Levy
  */
 public class ConstructorGeneration extends Visitor {
 
+    /**
+     * Creates an {@link ast.classbody.InitDecl} for the current class.
+     * @param cd Class Declaration
+     */
     public void visitClassDecl(ClassDecl cd) {
         Vector<AssignStmt> initParams = new Vector<>();
 
+        // For every field declared in the current class, we will create an
+        // assignment statement that will be stored in the generated constructor.
         for(String key : cd.symbolTable.getAllNames().keySet()) {
             if(cd.symbolTable.findName(key).decl().isFieldDecl()) {
                 FieldDecl currFieldDecl = cd.symbolTable.findName(key).decl().asFieldDecl();
                 initParams.add(
                     new AssignStmtBuilder()
-                            .setLHS(
-                                new FieldExprBuilder()
-                                        .setTarget(new NameExpr("this"))
-                                        .setAccessExpr(new NameExpr(currFieldDecl.toString()))
+                        .setLHS(
+                            new FieldExprBuilder()
+                                .setTarget(new ThisStmt())
+                                .setAccessExpr(
+                                    new NameExprBuilder()
+                                        .setName(currFieldDecl.var().name())
                                         .create()
-                            )
-                            .setRHS(currFieldDecl.var().init())
-                            .setAssignOp(
-                                    new AssignOp.AssignOpBuilder()
-                                            .setAssignOperator(AssignType.EQ)
-                                            .create())
-                            .create()
-                );
+                                )
+                                .create()
+                        )
+                        .setRHS(currFieldDecl.var().init())
+                        .setAssignOp(
+                            new AssignOpBuilder()
+                                .setAssignOperator(AssignType.EQ)
+                                .create()
+                        )
+                        .create());
             }
         }
-        cd.setConstructor(new InitDecl(initParams));
+
+        cd.setConstructor(
+            new InitDeclBuilder()
+                .setInits(initParams)
+                .create()
+        );
     }
 }
