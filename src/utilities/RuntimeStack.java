5e90da5c97595cc2b2416dc877a93640dc0a0146
Dalev <dalevyy@gmail.com>
2025-07-05 20:20:25 -0700

    Updated documentation and refactored class to work with new `Value` type.

--- src/utilities/RuntimeStack.java
+++ src/utilities/RuntimeStack.java
@@ -1,36 +1,126 @@
 package utilities;
 
+import ast.AST;
+import interpreter.value.Value;
 import java.util.HashMap;
 
+/**
+ * A class that mimics the behavior of a system stack.
+ * <p><br>
+ *     Since C Minor has an interpretation mode, we need to mimic the
+ *     behavior of a real system stack when users execute programs through
+ *     the {@link interpreter.VM}. This class is similar to the {@link SymbolTable}
+ *     class, but it is designed to work with the {@link interpreter.Interpreter} in
+ *     order to make it easier to access and save values during runtime.
+ * </p>
+ * @author Daniel Levy
+ */
 public class RuntimeStack {
 
-    private HashMap<String, Object> stack;
+    /**
+     * Internal hash map that will act as our stack.
+     */
+    private final HashMap<String, Value> stack;
+
+    /**
+     * Previous call frame that the current call frame originates from.
+     */
     private RuntimeStack parent;
 
+    /**
+     * Creates a default {@link RuntimeStack} object.
+     */
     public RuntimeStack() {
-        stack = new HashMap<String,Object>();
-        parent = null;
+        this.stack = new HashMap<>();
+        this.parent = null;
     }
 
+    /**
+     * Creates a {@link RuntimeStack} object that acts as a new call frame.
+     * @param parent {@link RuntimeStack} that the call frame points to
+     */
     public RuntimeStack(RuntimeStack parent) {
         this();
         this.parent = parent;
     }
 
-    public void addValue(String name, Object value) { stack.put(name,value); }
+    /**
+     * Adds a new value to the stack based on a variable name.
+     * @param name Variable name
+     * @param val {@link Value}
+     */
+    public void addValue(String name, Value val) { stack.put(name,val); }
 
-    public void setValue(String name, Object value) {
-        if(stack.containsKey(name)) { stack.put(name,value); }
-        else if(parent != null) { parent.setValue(name,value); }
+    /**
+     * Adds a new value to the stack based on an AST node.
+     * @param node AST node
+     * @param val {@link Value}
+     */
+    public void addValue(AST node, Value val) { addValue(node.toString(),val); }
+
+    /**
+     * Checks if the stack is storing a variable.
+     * @param name Variable name
+     * @return Boolean
+     */
+    private boolean hasValue(String name) {
+        if(stack.containsKey(name))
+            return true;
+        else if(parent != null)
+            return parent.hasValue(name);
+        else
+            return false;
     }
 
-    public Object getValue(String name) {
-        if(stack.containsKey(name)) { return stack.get(name); }
-        else if(parent != null) { return parent.getValue(name); }
-        else { return null; }
+    /**
+     * Sets the value for a variable on the stack.
+     * @param name Variable name we want to set a new value for
+     * @param value {@link Value} to save
+     */
+    public void setValue(String name, Value value) {
+        if(stack.containsKey(name))
+            stack.put(name,value);
+        else if(parent != null)
+            parent.setValue(name,value);
     }
 
+    /**
+     * Sets the value for an AST node on the stack.
+     * @param node AST node
+     * @param value {@link Value} to save
+     */
+    public void setValue(AST node, Value value) { setValue(node.toString(),value); }
+
+    /**
+     * Retrieves a value from the stack.
+     * @param name String representing the name we want to get a value from
+     * @return {@link Value}
+     */
+    public Value getValue(String name) {
+        if(stack.containsKey(name))
+            return stack.get(name);
+        else if(parent != null)
+            return parent.getValue(name);
+        else
+            return null;
+    }
+
+    /**
+     * Retrieves a value from the stack for an AST node.
+     * @param node AST node
+     * @return {@link Value}
+     */
+    public Value getValue(AST node) { return this.getValue(node.toString()); }
+
+    /**
+     * Creates a new stack call frame.
+     * @return {@link RuntimeStack}
+     */
     public RuntimeStack createCallFrame() { return new RuntimeStack(this);}
-    public RuntimeStack destroyCallFrame() { return this.parent; }
 
+    /**
+     * Destroys the current stack call frame.
+     * @return {@link RuntimeStack} representing the parent call frame
+     */
+    public RuntimeStack destroyCallFrame() { return this.parent; }
 }
