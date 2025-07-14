2f0e3d2a5c7f89f76f614ed1385db46bde2bf645
Dalev <dalevyy@gmail.com>
2025-07-14 16:57:17 -0700

    Added `printAllMessages` in order to generate all messages when running in compilation mode.

--- src/messages/Message.java
+++ src/messages/Message.java
@@ -1,6 +1,7 @@
 package messages;
 
 import ast.AST;
+import utilities.Vector;
 
 public abstract class Message {
 
@@ -32,9 +33,31 @@ public abstract class Message {
 
     public void setInterpretMode(boolean mode) { this.interpretMode = mode; }
 
+    public boolean isError() { return false; }
+    public boolean isWarning() { return false; }
+
     public String fileHeader() {
         if(fileName != null && !fileName.isEmpty())
             return "In " + fileName + ": ";
         return "";
+    }
+
+    public static boolean printAllMessages(Vector<Message> msgs) {
+        if(msgs.isEmpty())
+            return false;
+
+        // Print out every message to the terminal. This will be done in sequential order
+        // based on the order in which the messages are generated in.
+        for(Message msg : msgs)
+            System.out.println(msg.createMessage());
+
+        // We will only terminate the compilation process when an error is found. This looks
+        // through the messages vector to make sure we actually had an error as suppose to just
+        // a bunch of errors. This might be slow... not sure. :)
+        for(Message msg : msgs)
+            if(msg.isError())
+                return true;
+
+        return false;
     }
 }
