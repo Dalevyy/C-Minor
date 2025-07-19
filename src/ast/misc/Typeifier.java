9c775ffbae92de2c5b8eec2c933831914679ab00
Dalev <dalevyy@gmail.com>
2025-07-18 23:37:21 -0700

    Updated documentation.

--- src/ast/misc/Typeifier.java
+++ src/ast/misc/Typeifier.java
@@ -2,19 +2,22 @@ package ast.misc;
 
 import ast.AST;
 import ast.types.Type;
-import token.*;
-import utilities.*;
+import token.Token;
+import utilities.Visitor;
 
-/*
-___________________________ Typeifier ___________________________
-A typeifier node is used to keep track of templated types when
-working with templated classes/functions. (NOT YET IMPLEMENTED)
-_________________________________________________________________
-*/
+/**
+ * Type parameters for a template class or function.
+ * <p><br>
+ *     This {@link AST} subtype represents the type parameters that appear at the
+ *     beginning of a template class or function (i.e. {@code class A<discr T>} and
+ *     {@code def func<scalar S, discr T>}).
+ * </p>
+ * @author Daniel Levy
+ */
 public class Typeifier extends AST implements NameNode {
 
     /**
-     * List of potential types the typefier could represent.
+     * List of potential types the type parameter could represent.
      */
     public enum TypeAnnotation { DISCR, SCALAR, CLASS }
 
@@ -24,14 +27,13 @@ public class Typeifier extends AST implements NameNode {
     public static String[] names = {"discrete", "scalar", "class" };
 
     /**
-     * The high level type that the typefier could represent.
-     * <p>
-     *     In C Minor, only 3 classes of types are allowed to be used
-     *     within templates: discrete, scalar, and class. The user can
-     *     choose to specify if the typefier is any of these 3 types;
-     *     however, it is not a requirement. As a result, this field
-     *     will be empty, and it will be our job to make sure the user
-     *     correctly uses the appropriate type.
+     * The high level type that the type parameter could represent.
+     * <p><br>
+     *     In C Minor, only 3 classes of types are allowed to be used within templates:
+     *     discrete, scalar, and class. The user can choose to specify if the type parameter
+     *     is any of these 3 types; however, it is not a requirement. As a result, this field
+     *     is not guaranteed to be set which means any type can be used as long as no type
+     *     errors occur when the {@link typechecker.TypeChecker} executes.
      * </p>
      */
     private TypeAnnotation typeAnnotate;
@@ -41,11 +43,15 @@ public class Typeifier extends AST implements NameNode {
      */
     private Name name;
 
-    public Typeifier() { this(new Token(),null,null); }
     /**
-     * Default constructor for {@code Typeifier}
+     * Default constructor for {@code Typeifier}.
+     */
+    public Typeifier() { this(new Token(),null,null); }
+
+    /**
+     * Main constructor for {@code Typeifier}.
      * @param metaData Metadata token
-     * @param typeAnnotate The type the typefier represents
+     * @param typeAnnotate The potential type that the type parameter can accept
      * @param n The generic name for the type
      */
     public Typeifier(Token metaData, TypeAnnotation typeAnnotate, Name n) {
@@ -56,45 +62,86 @@ public class Typeifier extends AST implements NameNode {
         addChild(this.name);
     }
 
-    public TypeAnnotation getPossibleType() { return typeAnnotate; }
-    public Name getName() { return name; }
+    /**
+     * Checks if a passed type can be used with the current type parameter.
+     * <p><br>
+     *     This method will check the value of {@link #typeAnnotate} in order to
+     *     determine whether a passed type can be assigned to the type parameter.
+     *     If the {@link #typeAnnotate} is not set, then by default, the type
+     *     can be used with the current type parameter.
+     * </p>
+     * @param typeArg {@link Type} argument we want to assign to the type parameter
+     * @return True if the type argument can be used with the current type parameter, False otherwise
+     */
+    public boolean isValidTypeArg(Type typeArg) {
+        if(typeAnnotate == null)
+            return true;
 
-    public boolean isTypeifier() { return true; }
-    public Typeifier asTypeifier() { return this; }
-
-    public boolean hasTypeAnnotation() { return typeAnnotate != null; }
-
-    public boolean isValidTypeArg(Type t) {
-        switch(possibleTypeToString()) {
-            case "discrete":
-                if(t.isDiscreteType())
-                    return true;
-                break;
-            case "scalar":
-                if(t.isScalarType())
-                    return true;
-                break;
-            case "class":
-                if(t.isClassOrMultiType())
-                    return true;
-        }
-        return false;
+        return switch (typeAnnotate) {
+            case DISCR -> typeArg.isDiscreteType();
+            case SCALAR -> typeArg.isScalarType();
+            case CLASS -> typeArg.isClassOrMultiType();
+        };
     }
 
+    /**
+     * Returns the type annotation of the current type parameter.
+     * @return The value of {@link #typeAnnotate} as a {@link TypeAnnotation}.
+     */
+    public TypeAnnotation getPossibleType() { return typeAnnotate; }
+
+    /**
+     * Returns the name of the type parameter.
+     * @return The {@link Name} of the current type parameter.
+     */
+    public Name getName() { return name; }
+
+    /**
+     * Returns the {@link #typeAnnotate} field as a String.
+     * @return The string representation of {@link #typeAnnotate}.
+     */
     public String possibleTypeToString() { return names[typeAnnotate.ordinal()]; }
 
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isTypeifier() { return true; }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Typeifier asTypeifier() { return this; }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public AST decl() { return this; }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void update(int pos, AST n) { name = n.asName(); }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String header() { return getParent().header(); }
 
+    /**
+     * {@inheritDoc}
+     * @return String representation of the type parameter's {@link #name}.
+     */
     @Override
     public String toString() { return name.toString(); }
 
+    /**
+     * Checks if two type parameters have the same name.
+     * @param otherName String representing a type that might contain a type parameter
+     * @return True if the type parameters have the same name, False otherwise
+     */
     public boolean equals(String otherName) {
         String curr = name.toString();
         if(otherName.contains("<")) {
@@ -103,6 +150,10 @@ public class Typeifier extends AST implements NameNode {
         return curr.equals(otherName);
     }
 
+    /**
+     * {@inheritDoc}
+     * @return A deep copy of the current {@link Typeifier}.
+     */
     @Override
     public AST deepCopy() {
         return new TypeifierBuilder()
@@ -113,39 +164,60 @@ public class Typeifier extends AST implements NameNode {
     }
 
     /**
-     * Visit method.
-     * @param v Current visitor
+     * {@inheritDoc}
+     * @param v Current {@code Visitor} we are visiting.
      */
     @Override
     public void visit(Visitor v) { v.visitTypeifier(this); }
 
+    /**
+     * Internal class that builds a {@link Typeifier} object.
+     */
     public static class TypeifierBuilder extends NodeBuilder {
-        private final Typeifier tp = new Typeifier();
 
         /**
-         * Copies the metadata of an existing AST node into the builder.
-         * @param node AST node we want to copy.
-         * @return TypeifierBuilder
+         * {@link Typeifier} object we are building.
+         */
+        private final Typeifier typeParam = new Typeifier();
+
+        /**
+         * {@inheritDoc}
+         * @param node {@link AST} node we want to copy the metadata of.
+         * @return Current instance of {@link TypeifierBuilder}.
          */
         public TypeifierBuilder setMetaData(AST node) {
             super.setMetaData(node);
             return this;
         }
 
-        public TypeifierBuilder setPossibleType(TypeAnnotation pt) {
-            tp.typeAnnotate = pt;
+        /**
+         * Sets the typeifier's {@link #typeAnnotate}.
+         * @param typeAnnotate The generic type that the type parameter can be set to.
+         * @return Current instance of {@link TypeifierBuilder}.
+         */
+        public TypeifierBuilder setPossibleType(TypeAnnotation typeAnnotate) {
+            typeParam.typeAnnotate = typeAnnotate;
             return this;
         }
 
+        /**
+         * Sets the typeifier's {@link #name}.
+         * @param name The name that will be given to use the type parameter.
+         * @return Current instance of {@link TypeifierBuilder}.
+         */
         public TypeifierBuilder setName(Name name) {
-            tp.name = name;
+            typeParam.name = name;
             return this;
         }
 
+        /**
+         * Creates a {@link Typeifier} object.
+         * @return {@link Typeifier} object.
+         */
         public Typeifier create() {
-            super.saveMetaData(tp);
-            tp.addChild(tp.name);
-            return tp;
+            super.saveMetaData(typeParam);
+            typeParam.addChild(typeParam.name);
+            return typeParam;
         }
     }
 }
