092d26eff664e33a3d738b37f01dddca16aac18d
Dalev <dalevyy@gmail.com>
2025-07-31 16:25:47 -0700

    Turned `ErrorFactory` interface into an abstract class.

--- src/messages/errors/ErrorFactory.java
+++ src/messages/errors/ErrorFactory.java
@@ -1,5 +1,44 @@
 package messages.errors;
 
-public interface ErrorFactory {
-    public Error createError();
+import ast.misc.Compilation;
+
+public abstract class ErrorFactory {
+
+    /**
+     * Flag that denotes which mode the compiler runs in.
+     * <ul>
+     *     <li> Set to {@code True} if we are compiling with the {@link interpreter.VM}.</li>
+     *     <li> Set to {@code False} if we are compiling with the {@link compiler.Compiler}.</li>
+     * </ul>
+     */
+    protected boolean interpretationMode = false;
+
+    /**
+     * The name of the file we are generating an error for (if we are in compilation mode).
+     */
+    protected String fileName = "";
+
+    /**
+     * Generates a new {@link Error} object based on the factory type.
+     * @return An {@link Error} dependent on the current factory.
+     */
+    public abstract Error createError();
+
+    /**
+     * Setter responsible for setting {@link #interpretationMode} to be {@code True} when called.
+     * <p>
+     *     This setter is only called when we are in interpretation mode.
+     * </p>
+     */
+    public void setInterpretationExecutionMode() { this.interpretationMode = true; }
+
+    /**
+     * Setter responsible for setting the {@link #fileName}.
+     * <p>
+     *     This setter is only called during a {@link utilities.Visitor#visitCompilation(Compilation)} execution
+     *     whenever the compiler is in compilation mode for each respective visitor.
+     * </p>
+     * @param fileName String representing the file name that will be used when generating an error.
+     */
+    public void setFileName(String fileName) { this.fileName = fileName; }
 }
