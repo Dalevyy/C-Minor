3403f211aebbd883ee0bdd37d3540c9a9e3bb1b5
Dalev <dalevyy@gmail.com>
2025-08-03 11:49:32 -0700

    Refactored `Modifier` class to be more straightforward to work with.

--- src/ast/misc/Modifier.java
+++ src/ast/misc/Modifier.java
@@ -1,49 +1,118 @@
 package ast.misc;
 
 import ast.AST;
-import token.*;
+import token.Token;
 import utilities.Visitor;
 
-/*
-__________________________ Modifier __________________________
-A Modifier node will contain a modifier that a user specified
-for a given C Minor construct. Currently, modifiers can be
-applied to ClassDecls, FieldDecls, MethodDecls, FuncDecls, and
-ParamDecls.
-______________________________________________________________
-*/
-public class Modifier extends AST {
+/**
+ * An {@link AST} node that stores all modifiers associated with a given node.
+ * <p>
+ *     This class serves as a helper for us to know the access privileges that
+ *     various other {@link AST} nodes will have. As a result, it will not be
+ *     stored within the parse tree itself.
+ * </p>
+ * @author Daniel Levy
+ */
+public class Modifier extends SubNode {
 
-    public enum Mods { ABSTR, FINAL, PROPERTY, PROTECTED, PUBLIC, PURE, RECURS, IN, OUT, INOUT, REF }
-    public static String[] names = {"Abstract", "Final", "Property", "Protected", "Public", "Pure",
-                                    "Recursive", "In", "Out", "Inout", "Ref" };
-    private final Mods mod;
+    public boolean isPublic = false;
+    public boolean isProtected = false;
+    public boolean isProperty = false;
+    public boolean isFinal = false;
+    public boolean isAbstract = false;
+    public boolean isPure = false;
+    public boolean isRecursive = false;
+    public boolean isInMode = false;
+    public boolean isOutMode = false;
+    public boolean isInOutMode = false;
+    public boolean isRefMode = false;
 
-    public Modifier(Mods m) { this.mod = m; }
+    public Modifier() { super(new Token()); }
 
-    public Modifier(Token t, Mods m) {
-        super(t);
-        this.mod = m;
-    }
+    /**
+     * Sets {@link #isPublic} to be {@code True}.
+     */
+    public void setPublic() { isPublic = true; }
 
-    public Mods getModifier() { return mod; }
+    /**
+     * Sets {@link #isProtected} to be {@code True}.
+     */
+    public void setProtected() { isProtected = true; }
 
+    /**
+     * Sets {@link #isProperty} to be {@code True}.
+     */
+    public void setProperty() { isProperty = true; }
+
+    /**
+     * Sets {@link #isFinal} to be {@code True}.
+     */
+    public void setFinal() { isFinal = true; }
+
+    /**
+     * Sets {@link #isAbstract} to be {@code True}.
+     */
+    public void setAbstract() { isAbstract = true; }
+
+    /**
+     * Sets {@link #isPure} to be {@code True}.
+     */
+    public void setPure() { isPure = true; }
+
+    /**
+     * Sets {@link #isRecursive} to be {@code True}.
+     */
+    public void setRecursive() { isRecursive = true; }
+
+    /**
+     * Sets {@link #isInMode} to be {@code True}.
+     */
+    public void setInMode() { isInMode = true; }
+
+    /**
+     * Sets {@link #isOutMode} to be {@code True}.
+     */
+    public void setOutMode() { isOutMode = true; }
+
+    /**
+     * Sets {@link #isInOutMode} to be {@code True}.
+     */
+    public void setInOutMode() { isInOutMode = true; }
+
+    /**
+     * Sets {@link #isRefMode} to be {@code True}.
+     */
+    public void setRefMode() { isRefMode = true; }
+
+    /**
+     * {@inheritDoc}
+     */
     public boolean isModifier() { return true; }
+
+    /**
+     * {@inheritDoc}
+     */
     public Modifier asModifier() { return this; }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public String toString() { return names[mod.ordinal()]; }
+    public void update(int pos, AST node) { throw new RuntimeException("A modifier can not be updated."); }
 
-    @Override
-    public void update(int pos, AST n) { throw new RuntimeException("A modifier can not be updated."); }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public AST deepCopy() {
-        Modifier m = new Modifier(this.mod);
-        m.copyMetaData(this);
-        return m;
+        Modifier copy = new Modifier();
+        copy.copyMetaData(this);
+        return copy;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void visit(Visitor v){ v.visitModifier(this); }
 }
