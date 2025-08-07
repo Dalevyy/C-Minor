427e409f5bec17cedebc06f07d12b7e97b8c5d07
Dalev <dalevyy@gmail.com>
2025-08-07 16:33:02 -0700

    Updated documentation.

--- src/utilities/PrettyPrint.java
+++ src/utilities/PrettyPrint.java
@@ -2,7 +2,7 @@ package utilities;
 
 /**
  * Formatting class to help display messages to the user.
- * <p><br>
+ * <p>
  *     This class contains a list of ANSI escape sequences I found online
  *     for different text formatting and coloring options. I wanted to add
  *     this to help color code errors and warnings. In the future, a better
@@ -11,13 +11,13 @@ package utilities;
  * @author Daniel Levy
  */
 public final class PrettyPrint {
-    // General
+    /* ################################# GENERAL ################################# */
     public static final String RESET = "\u001B[0m";
     public static final String BOLD = "\u001B[22m";
     public static final String ITALIC = "\u001B[23m";
     public static final String LINE = "\u001B[24m";
 
-    // Foreground Colors
+    /* ################################# FOREGROUND COLORS ################################# */
     public static final String BLACK = "\u001B[30m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
@@ -28,7 +28,7 @@ public final class PrettyPrint {
     public static final String WHITE = "\u001B[37m";
     public static final String PINK = "\033[38;5;206m";
 
-    // Background Colors
+    /* ################################# BACKGROUND COLORS ################################# */
     public static final String bBLACK = "\u001B[40m";
     public static final String bRED = "\u001B[41m";
     public static final String bGREEN = "\u001B[42m";
@@ -37,5 +37,4 @@ public final class PrettyPrint {
     public static final String bPURPLE = "\u001B[45m";
     public static final String bCYAN = "\u001B[46m";
     public static final String bWHITE = "\u001B[47m";
-
 }
