10cdbdbdd7206b70a9fe869acb489138b031777f
Dalev <dalevyy@gmail.com>
2025-08-14 13:50:23 -0700

    Updated `ArrayType` to be the supertype for `ListType`.

--- src/main/java/cminor/ast/types/ArrayType.java
+++ src/main/java/cminor/ast/types/ArrayType.java
@@ -5,8 +5,8 @@ import cminor.token.Token;
 import cminor.utilities.Visitor;
 
 /**
- * A structured type representing an array.
- * <p><br>
+ * A structured {@link Type} representing an array.
+ * <p>
  *     In C Minor, an array represents a block of continuous memory
  *     that is statically sized. During compilation, the size of an
  *     array must be known, and the user is not able to change the
@@ -16,63 +16,87 @@ import cminor.utilities.Visitor;
  */
 public class ArrayType extends Type {
 
-    private Type baseType;
-    public int numOfDims;
+    /**
+     * The number of dimensions represented by the current {@link ArrayType}.
+     */
+    protected int dims;
 
+    /**
+     * The {@link Type} that the actual elements inside the array represents.
+     */
+    protected Type baseType;
+
+    /**
+     * Default constructor for {@link ArrayType}.
+     */
     public ArrayType() { this(new Token(),null,0); }
-    public ArrayType(Type bt, int num) { this(new Token(),bt,num); }
-    public ArrayType(Token t, Type bt, int num) {
-        super(t);
-        this.baseType = bt;
-        this.numOfDims = num;
+
+    /**
+     * Main constructor for {@link ArrayType}.
+     * @param metaData {@link Token} containing all the metadata we will save into this node.
+     * @param baseType {@link Type} to store into {@link #baseType}.
+     * @param dims {@code int} to store into {@link #dims}.
+     */
+    public ArrayType(Token metaData, Type baseType, int dims) {
+        super(metaData);
+        this.baseType = baseType;
+        this.dims = dims;
     }
 
-    public Type baseType() { return baseType; }
+    /**
+     * Getter method for {@link #dims}.
+     * @return The number of dimensions the {@link ArrayType} has.
+     */
+    public int getDims() { return dims; }
 
-    public int getNumOfDims() {
-        return numOfDims;
-    }
+    /**
+     * Getter method for {@link #baseType}.
+     * @return The {@link Type} that the array's elements stores.
+     */
+    public Type getBaseType() { return baseType; }
 
-    public void setBaseType(Type baseType) { this.baseType = baseType;}
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isArray() { return true; }
 
-    private void setNumOfDims(int numOfDims) {
-        this.numOfDims = numOfDims;
-    }
+    /**
+     * {@inheritDoc}
+     */
+    public ArrayType asArray() { return this; }
 
-    public boolean isArrayType() { return true; }
-    public ArrayType asArrayType() { return this; }
-
-    @Override
-    public String typeName() {
+    /**
+     * {@inheritDoc}
+     */
+    public String getTypeName() {
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i <= numOfDims; i++) {
-            if(i == numOfDims)
-                sb.append(baseType.typeName());
+        for(int i = 0; i <= dims; i++) {
+            if(i == dims)
+                sb.append(baseType.getTypeName());
             else
                 sb.append("Array[");
         }
 
-        sb.append("]".repeat(Math.max(0, numOfDims)));
+        sb.append("]".repeat(Math.max(0, dims)));
         return sb.toString();
     }
 
-    @Override
-    public String toString() { return typeName(); }
-
     /**
-     * {@code deepCopy} method.
-     * @return Deep copy of the current {@link ArrayType}
+     * {@inheritDoc}
      */
     @Override
     public AST deepCopy() {
         return new ArrayTypeBuilder()
                    .setMetaData(this)
-                   .setBaseType(this.baseType.deepCopy().asType())
-                   .setNumOfDims(this.numOfDims)
+                   .setBaseType(baseType.deepCopy().asType())
+                   .setNumOfDims(dims)
                    .create();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void visit(Visitor v) { v.visitArrayType(this); }
 
@@ -87,9 +111,8 @@ public class ArrayType extends Type {
         private final ArrayType at = new ArrayType();
 
         /**
-         * Copies the metadata of an existing AST node into the builder.
-         * @param node AST node we want to copy.
-         * @return ArrayTypeBuilder
+         * @see cminor.ast.AST.NodeBuilder#setMetaData(AST, AST)
+         * @return Current instance of {@link ArrayTypeBuilder}.
          */
         public ArrayTypeBuilder setMetaData(AST node) {
             super.setMetaData(at,node);
@@ -98,21 +121,21 @@ public class ArrayType extends Type {
 
         /**
          * Sets the array type's {@link #baseType}.
-         * @param baseType Type that represents the values stored by the array
-         * @return ArrayTypeBuilder
+         * @param baseType {@link Type} that represents the values stored by the array.
+         * @return Current instance of {@link ArrayTypeBuilder}.
          */
         public ArrayTypeBuilder setBaseType(Type baseType) {
-            at.setBaseType(baseType);
+            at.baseType = baseType;
             return this;
         }
 
         /**
-         * Sets the array type's {@link #numOfDims}.
-         * @param numOfDims Int representing how many dimensions the array has
-         * @return ArrayTypeBuilder
+         * Sets the array type's {@link #dims}.
+         * @param dims {@code Int} representing how many dimensions the array type has.
+         * @return Current instance of {@link ArrayTypeBuilder}.
          */
-        public ArrayTypeBuilder setNumOfDims(int numOfDims) {
-            at.setNumOfDims(numOfDims);
+        public ArrayTypeBuilder setNumOfDims(int dims) {
+            at.dims = dims;
             return this;
         }
 
