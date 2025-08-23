d21f3044dc76828ff8b6877a01e4ddab68587007
Dalev <dalevyy@gmail.com>
2025-08-23 09:44:20 -0700

    Set up testing framework for modifier tests.

---
+++ src/test/groovy/cminor/modifier/bad/ModifierBadTest.groovy
@@ -0,0 +1,9 @@
+package cminor.modifier.bad
+
+import cminor.messages.CompilationMessage
+import cminor.messages.MessageNumber
+import cminor.modifier.ModifierTest
+
+class ModifierBadTest extends ModifierTest {
+
+}
