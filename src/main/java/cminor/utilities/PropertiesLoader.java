44bb85cac53879cce6d3dd9d0125c7159b2c4454
Dalev <dalevyy@gmail.com>
2025-08-25 12:57:32 -0700

    Updated documentation.

--- src/main/java/cminor/utilities/PropertiesLoader.java
+++ src/main/java/cminor/utilities/PropertiesLoader.java
@@ -9,6 +9,11 @@ import java.util.Properties;
 // Dr. C's class from ProcessJ
 public class PropertiesLoader {
 
+    /**
+     * Finds the location of the properties file.
+     * @param fileName String representing the name of the file.
+     * @return {@link URL} for where the file is located.
+     */
     public static URL getURL(String fileName) {
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
         URL url = cl.getResource(fileName);
@@ -18,13 +23,18 @@ public class PropertiesLoader {
         return cl.getResource(fileName);
     }
 
+    /**
+     * Loads the properties file.
+     * @param file The {@link File} containing the properties.
+     * @return {@link Properties}
+     */
     public static Properties loadProperties(File file) {
         Properties p = new Properties();
         try {
             p.load(Files.newInputStream(file.toPath()));
         }
         catch(IOException e) {
-            System.out.println(file.toString());
+            System.out.println(file);
             System.out.println(PrettyPrint.RED + "There was an error loading the properties file... :(");
             System.exit(1);
         }
