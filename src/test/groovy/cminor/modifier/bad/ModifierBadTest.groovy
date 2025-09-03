51f2daaf706029bde0f5523e862c04986566ab29
Dalev <dalevyy@gmail.com>
2025-09-02 22:20:08 -0700

    Added modifier test cases.

--- src/test/groovy/cminor/modifier/bad/ModifierBadTest.groovy
+++ src/test/groovy/cminor/modifier/bad/ModifierBadTest.groovy
@@ -38,6 +38,34 @@ class ModifierBadTest extends ModifierTest {
             error.msg.messageType == MessageNumber.MOD_ERROR_500
     }
 
+    def "Function Declaration - Recursive Call on Non-Recursive Function"() {
+        when: "A recursive call is made inside of a function that wasn't marked recursive."
+            input = '''
+                        def factorial(in a:Int) => Int {
+                            if(a == 0) { return 1 }
+                            return a * factorial(a-1)
+                        }
+                    '''
+            vm.runInterpreter(input)
+
+        then: "An error needs to be generated since a runtime error could occur without the user knowing."
+            error = thrown CompilationMessage
+            error.msg.messageType == MessageNumber.MOD_ERROR_502
+    }
+
+    def "Global Declaration - Constant Value is Updated"() {
+        when: "A global constant is declared, and its value is updated."
+            input = '''
+                        def const a:Int = 3 
+                        set a = 5
+                    '''
+            vm.runInterpreter(input)
+
+        then: "An error should be thrown since a constant can not change its value."
+            error = thrown CompilationMessage
+            error.msg.messageType == MessageNumber.MOD_ERROR_505
+    }
+
     def "New Expression - Object Instantiated From Abstract Class"() {
         when: "An object is instantiated from an abstract class."
             input = '''
--- src/test/groovy/cminor/modifier/good/ModifierGoodTest.groovy
+++ src/test/groovy/cminor/modifier/good/ModifierGoodTest.groovy
@@ -5,4 +5,52 @@ import cminor.modifier.ModifierTest
 
 class ModifierGoodTest extends ModifierTest {
 
+    def "Class Declaration - Abstract Class Implementation"() {
+        when: "A class inherits from an abstract class."
+            input = '''
+                        abstr class AbstrClass {
+                            public method firstMethod() => Void {}
+                            public method secondMethod() => Void {}
+                        }
+
+                        class ConcreteClass inherits AbstrClass {
+                            public override method firstMethod() => Void {}
+                            public override method secondMethod() => Void {}
+                        }
+
+                        def obj:ConcreteClass = new ConcreteClass()
+                    '''
+            vm.runInterpreter(input)
+
+        then: "If all methods from the abstract class are implemented, then no errors will be generated."
+            notThrown CompilationMessage
+    }
+
+    def "Function Declaration - Recursive Function"() {
+        when: "A recursive call is written inside a function."
+            input = '''
+                        def recurs factorial(in a:Int) => Int {
+                            if(a == 0) { return 1 }
+                            return a * factorial(a-1)
+                        }
+                    '''
+            vm.runInterpreter(input)
+
+        then: "As long as the function itself is marked recursive, then no error is generated."
+            notThrown CompilationMessage
+    }
+
+    def "Field Expression - Accessing Public Field"() {
+        when: "A field is accessed outside a class."
+            input = ''' 
+                        class myClass { public myField:Int }
+                        
+                        def a:myClass = new myClass(myField=5)
+                        cout << a.myField
+                    '''
+            vm.runInterpreter(input)
+
+        then: "An error is not generated as long as the field is marked public."
+            notThrown CompilationMessage
+    }
 }
